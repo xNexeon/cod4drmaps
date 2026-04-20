a418e5ed0107df02d093e402badba9773ca619a5
drumsmokerghosty <amberleafhash2@gmail.com>
2026-04-21 00:13:30 +0100

    Delete deathrun_dev directory

--- mods/deathrun_dev/runDedicatedServer.bat
+++
@@ -1,3 +0,0 @@
-
-cd ..\..\
-iw3mp.exe +set fs_game "mods\deathrun_12" +set dedicated 2 +set net_port 28960 +set g_gametype "deathrun" +exec server.cfg +map mp_deathrun_long 
--- mods/deathrun_dev/runMod.bat
+++
@@ -1,4 +0,0 @@
-
-cd ..\..\
-iw3mp.exe +set fs_game "mods\deathrun_dev" +connect localhost
-pause
